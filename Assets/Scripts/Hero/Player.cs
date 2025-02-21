8e1c7f3762d693ef065dd4984589baa03116cdf9
h3k70 <ser0307z@gmail.com>
2025-02-21 19:13:51 +0300

    Optimization for android

--- Assets/Scripts/Hero/Player.cs
+++ Assets/Scripts/Hero/Player.cs
@@ -10,6 +10,10 @@ public class Player : MonoBehaviour
 
     private PlayerInput _inputs;
     [SerializeField] private Build _currentBuild;
+    private bool _isSwipeEnded = true;
+    private Vector2 _swipeStartPosition;
+    private float _minSwipeDistance = 70;
+
     private void Awake()
     {
         _move.Init(_currentBuild);
@@ -17,7 +21,43 @@ public class Player : MonoBehaviour
         _inputs = new PlayerInput();
 
         _inputs.Player.Attack.performed += context => _baseAttack.OnAttack();
-        _inputs.Player.Move.performed += context => _move.JumpToSpline(context.ReadValue<float>());
+        _inputs.Player.Move.started += context => OnSwipeStarted(context.ReadValue<Vector2>());
+        _inputs.Player.Move.performed += context => OnSwipe(context.ReadValue<Vector2>());
+        _inputs.Player.Move.canceled += context => OnSwipeEnded(context.ReadValue<Vector2>());
+    }
+
+    private void OnSwipeStarted(Vector2 dir)
+    {
+        _swipeStartPosition = dir;
+
+        if (dir.y == 1 || dir.y == -1)
+        {
+            _move.JumpToSpline(dir.y);
+        }
+    }
+
+    private void OnSwipe(Vector2 position)
+    {
+        Vector2 swipeDelta = position - _swipeStartPosition;
+
+        if (_isSwipeEnded && (swipeDelta).magnitude > _minSwipeDistance)
+        {
+            if (swipeDelta.y > 0)
+            {
+                _move.JumpToSpline(1);
+            }
+            else
+            {
+                _move.JumpToSpline(-1);
+            }
+            _isSwipeEnded = false;
+        }
+    }
+
+    private void OnSwipeEnded(Vector2 dir)
+    {
+        _swipeStartPosition = Vector2.zero;
+        _isSwipeEnded = true;
     }
 
     private void OnEnable()
